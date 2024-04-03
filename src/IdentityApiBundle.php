2ac120ee1e98690e585f9effe5382dab7c4a046b
Simon McWhinnie <simon.mcwhinnie@immediate.co.uk>
2024-04-03 14:41:45 +0100

    add service configuration

--- src/IdentityApiBundle.php
+++ src/IdentityApiBundle.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace IM\Fabric\Package\IdentityApiBundle;
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class IdentityApiBundle extends AbstractBundle
 {
-
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
+    }
 }
